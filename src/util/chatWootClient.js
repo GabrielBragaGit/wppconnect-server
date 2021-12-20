15364cb6ae6565ed60d8eb02324c3736f89100d4
GabrielBragaGit <65824599+GabrielBragaGit@users.noreply.github.com>
2021-12-20 15:46:05 -0300

    Update chatWootClient.js

--- src/util/chatWootClient.js
+++ src/util/chatWootClient.js
@@ -47,7 +47,7 @@ export default class chatWootClient {
           caption: 'leia o qrCode',
           qrCode: qrCode.replace('data:image/png;base64,', ''),
         });
-      });
+      }, 500);
     });
 
     //assiona o evento do status
@@ -58,7 +58,7 @@ export default class chatWootClient {
           chatId: '',
           body: `wppconnect status: ${status} `,
         });
-      });
+      }, 500);
     });
 
     //assina o evento de mensagem
