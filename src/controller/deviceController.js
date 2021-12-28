94b87d1baee52d9ac1742b42951512a48827408f
GabrielBragaGit <65824599+GabrielBragaGit@users.noreply.github.com>
2021-12-28 19:56:49 -0300

    Update deviceController.js

--- src/controller/deviceController.js
+++ src/controller/deviceController.js
@@ -716,13 +716,14 @@ export async function chatWoot(req, res) {
 
 }
 async function updateMessage(client, contact_id, conversation_id, message_id, message) {
-  console.log(client);
-  let api = axios.create({
-    baseURL: client.config.baseURL,
-    headers: { 'Content-Type': 'application/json;charset=utf-8', api_access_token: client.config.token },
-  });
+  console.log(client.config);
+  console.log(client.api);
+  // let api = axios.create({
+  //   baseURL: client.config.baseURL,
+  //   headers: { 'Content-Type': 'application/json;charset=utf-8', api_access_token: client.config.token },
+  // });
   try {
-    const { data } = await api.patch(
+    const { data } = await client.api.patch(
       `public/api/v1/inboxes/${client.config.inbox_identifier}/contacts/${contact_id}/conversations/${conversation_id}/messages/${message_id}`,
       { submitted_values: { value: message.id } });
     return data;
